b8d8b01fcebff80d9cd6b358d9c7c5740bf0bedc
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-06-29 22:34:04 +0300

    fix: do not attempt to cache the session, the "async with" will close it anyway

--- src/rmmtxauthz/mediamtx.py
+++ src/rmmtxauthz/mediamtx.py
@@ -2,7 +2,7 @@
 
 from typing import Optional, ClassVar, Sequence, Dict, Any
 import logging
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 import aiohttp
 
@@ -15,8 +15,6 @@ LOGGER = logging.getLogger(__name__)
 class MediaMTXControl:
     """MediaMTX control API abstraction"""
 
-    _session: Optional[aiohttp.ClientSession] = field(default=None, init=False, repr=False)
-
     _singleton: ClassVar[Optional["MediaMTXControl"]] = None
 
     @classmethod
@@ -28,12 +26,9 @@ class MediaMTXControl:
 
     def get_session(self) -> aiohttp.ClientSession:
         """Get session"""
-        if self._session:
-            return self._session
         cnf = RMMTXSettings.singleton()
         auth = aiohttp.BasicAuth(login=cnf.api_username, password=cnf.api_password)
-        self._session = aiohttp.ClientSession(auth=auth, base_url=cnf.api_url, raise_for_status=True)
-        return self._session
+        return aiohttp.ClientSession(auth=auth, base_url=cnf.api_url, raise_for_status=True)
 
     async def get_paths(self, insert_credentials: str = "") -> Sequence[Dict[str, Any]]:
         """Get active paths and generate their corresponding urls for each protocol
