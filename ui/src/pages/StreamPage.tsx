f39090f73c36ce3446af18c0511914e13ece31bd
vitamiini-0 <228652369+vitamiini-0@users.noreply.github.com>
2025-12-03 18:17:20 +0200

    feat: improve media MTX stream page UI

--- ui/src/pages/StreamPage.tsx
+++ ui/src/pages/StreamPage.tsx
@@ -34,16 +34,21 @@ export const StreamPage = () => {
   const [user, setUser] = useState<Userinfo | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState(false);
-  const [toolLinks, setToolLinks] = useState<StreamLinks[]>([]);
   const [advancedLinks, setAdvancedLinks] = useState<StreamLinks[]>([]);
 
   const [isAdvancedOpen, setIsAdvancedOpen] = useState(false);
+  const [isOpenTakIcuOpen, setIsOpenTakIcuOpen] = useState(false);
+  const [isGoProAppOpen, setIsGoProAppOpen] = useState(false);
+  const [showGoProAppLink, setShowGoProAppLink] = useState(false);
+  const [isUasToolOpen, setIsUasToolOpen] = useState(false);
   const [showToolPasswords, setShowToolPasswords] = useState<
     Record<number, boolean>
   >({});
   const [showAdvancedPasswords, setShowAdvancedPasswords] = useState<
     Record<number, boolean>
   >({});
+
+  const currentDomain = window.location.hostname.replace(/^mtls./, "");
 
   useEffect(() => {
     async function fetchCredentials() {
@@ -58,24 +63,6 @@ export const StreamPage = () => {
 
         const data: Userinfo = await response.json();
         setUser(data);
-
-        const currentDomain = window.location.hostname.replace(/^mtls./, "");
-
-        const links: StreamLinks[] = [
-          {
-            name: t("stream.opentak_icu"),
-            url: `rtmps://${currentDomain}:1936/live/icu/${data.username}`,
-          },
-          {
-            name: t("stream.gopro_app"),
-            url: `rtmps://${currentDomain}:1936/live/gopro/${data.username}?user=${data.username}&pass=${data.password}`,
-            hideCredentials: true,
-          },
-          {
-            name: t("stream.uastool"),
-            url: `rtsp://${currentDomain}:8554/live/uas/${data.username}`,
-          },
-        ];
 
         const advLinks: StreamLinks[] = [
           {
@@ -98,7 +85,6 @@ export const StreamPage = () => {
           },
         ];
 
-        setToolLinks(links);
         setAdvancedLinks(advLinks);
       } catch (err: any) {
         console.error("Error fetching credentials:", err);
@@ -127,29 +113,32 @@ export const StreamPage = () => {
             <div className="mt-6 space-y-6">
               <div className="text-left">
                 <Label className="font-semibold"><TranslatedText id="stream.passw"/></Label>
-                <div className="flex gap-2 mt-1">
+                <div className="flex flex-col md:flex-row gap-2 mt-1">
                   <Input readOnly value={user.username} />
-                  <Button onClick={() => copyToClipboard(user.username, t("common.copied"))}>
-                    <TranslatedText id="common.copy"/> <Copy />
-                  </Button>
+                  <div>
+                    <Button onClick={() => copyToClipboard(user.username, t("common.copied"))}>
+                      <TranslatedText id="common.copy"/> <Copy />
+                    </Button>
+                  </div>
                 </div>
               </div>
-
               <div className="text-left">
                 <Label className="font-semibold"><TranslatedText id="stream.username"/></Label>
-                <div className="flex gap-2 mt-1">
+                <div className="flex flex-col md:flex-row gap-2 mt-1">
                   <Input
                     readOnly
                     type={showPassword ? "text" : "password"}
                     value={user.password}
                   />
-                  <Button onClick={() => setShowPassword(!showPassword)}>
-                    {showPassword ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}
-                    {showPassword ? <EyeClosed /> : <Eye />}
-                  </Button>
-                  <Button onClick={() => copyToClipboard(user.password, t("common.copied"))}>
-                    <TranslatedText id="common.copy"/> <Copy />
-                  </Button>
+                  <div className="flex flex-row gap-2 mt-1">
+                    <Button onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}
+                      {showPassword ? <EyeClosed /> : <Eye />}
+                    </Button>
+                    <Button onClick={() => copyToClipboard(user.password, t("common.copied"))}>
+                      <TranslatedText id="common.copy"/> <Copy />
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -158,42 +147,120 @@ export const StreamPage = () => {
               <p className="font-semibold text-xl text-left"><TranslatedText id="stream.tools"/></p>
 
               {/* Tools */}
-              {toolLinks.map((link, idx) => (
-                <div key={idx} className="pt-4 text-left">
-                  <p className="font-bold text-gray-800">{link.name}</p>
-                  <div className="flex gap-2 mt-2">
-                    <Input
-                      readOnly
-                      type={
-                        link.hideCredentials && !showToolPasswords[idx]
-                          ? "password"
-                          : "text"
-                      }
-                      value={link.url}
-                      className="flex-1"
-                    />
+              <div className="mt-8 border rounded-lg p-4 text-left">
+                <button
+                  className="flex items-center justify-between w-full font-semibold text-lg"
+                  onClick={() => setIsOpenTakIcuOpen(!isOpenTakIcuOpen)}
+                >
+                  <TranslatedText id="stream.opentak_icu" className="text-left"/>
+                  {isOpenTakIcuOpen ? <ChevronUp /> : <ChevronDown />}
+                </button>
 
-                    {link.hideCredentials && (
-                      <Button
-                        onClick={() =>
-                          setShowToolPasswords((prev) => ({
-                            ...prev,
-                            [idx]: !prev[idx],
-                          }))
-                        }
-                      >
-                        {showToolPasswords[idx] ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}{" "}
-                        {showToolPasswords[idx] ? <EyeClosed /> : <Eye />}
-                      </Button>
-                    )}
-
-                    <Button onClick={() => copyToClipboard(link.url, t("common.copied"))}>
-                      <TranslatedText id="common.copy"/> <Copy />
-                    </Button>
+                {isOpenTakIcuOpen && (
+                  <div className="mt-4 space-y-4">
+                    <div className="text-left">
+                      <TranslatedText id="stream.protocol" className="font-bold text-gray-800"/>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input readOnly value={"RTMPS"} className="flex-1"/>
+                        <div>
+                          <Button onClick={() => copyToClipboard("RTMPS", t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-left">
+                      <TranslatedText id="stream.address" className="font-bold text-gray-800"/>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input readOnly value={currentDomain} className="flex-1"/>
+                        <div>
+                          <Button onClick={() => copyToClipboard(currentDomain, t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-left">
+                      <TranslatedText id="stream.port" className="font-bold text-gray-800"/>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input readOnly value={"1936"} className="flex-1"/>
+                        <div>
+                          <Button onClick={() => copyToClipboard("1936", t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="text-left">
+                      <TranslatedText id="stream.path" className="font-bold text-gray-800"/>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input readOnly value={`/live/icu/${user.username}`} className="flex-1"/>
+                        <div>
+                          <Button onClick={() => copyToClipboard(`/live/icu/${user.username}`, t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              ))}
-
+                )}
+              </div>
+              <div className="mt-8 border rounded-lg p-4 text-left">
+                <button
+                  className="flex items-center justify-between w-full font-semibold text-lg"
+                  onClick={() => setIsGoProAppOpen(!isGoProAppOpen)}
+                >
+                  <TranslatedText id="stream.gopro_app" className="text-left"/>
+                  {isOpenTakIcuOpen ? <ChevronUp /> : <ChevronDown />}
+                </button>
+                {isGoProAppOpen && (
+                  <div className="mt-4 space-y-4">
+                    <div className="text-left">
+                      <Label className="font-semibold"><TranslatedText id="stream.address"/></Label>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input
+                          readOnly
+                          type={showGoProAppLink ? "text" : "password"}
+                          value={`rtmps://${currentDomain}:1936/live/gopro/${user.username}?user=${user.username}&pass=${user.password}`}
+                        />
+                        <div className="flex flex-row gap-2 mt-1">
+                          <Button onClick={() => setShowGoProAppLink(!showGoProAppLink)}>
+                            {showGoProAppLink ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}
+                            {showGoProAppLink ? <EyeClosed /> : <Eye />}
+                          </Button>
+                          <Button onClick={() => copyToClipboard(`rtmps://${currentDomain}:1936/live/gopro/${user.username}?user=${user.username}&pass=${user.password}`, t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="mt-8 border rounded-lg p-4 text-left">
+                <button
+                  className="flex items-center justify-between w-full font-semibold text-lg"
+                  onClick={() => setIsUasToolOpen(!isUasToolOpen)}
+                >
+                  <TranslatedText id="stream.uastool" className="text-left"/>
+                  {isUasToolOpen ? <ChevronUp /> : <ChevronDown />}
+                </button>
+                {isUasToolOpen && (
+                  <div className="mt-4 space-y-4">
+                    <div className="text-left">
+                      <TranslatedText id="stream.address" className="font-bold text-gray-800"/>
+                      <div className="flex flex-col md:flex-row gap-2 mt-1">
+                        <Input readOnly value={`rtsp://${currentDomain}:8554/live/uas/${user.username}`} className="flex-1"/>
+                        <div>
+                          <Button onClick={() => copyToClipboard(`rtsp://${currentDomain}:8554/live/uas/${user.username}`, t("common.copied"))}>
+                            <TranslatedText id="common.copy"/> <Copy />
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
               {/* Advanced */}
               <div className="mt-8 border rounded-lg p-4 text-left">
                 <button
@@ -209,7 +276,7 @@ export const StreamPage = () => {
                     {advancedLinks.map((link, idx) => (
                       <div key={idx} className="text-left">
                         <p className="font-bold text-gray-800">{link.name}</p>
-                        <div className="flex gap-2 mt-2">
+                        <div className="flex flex-col md:flex-row gap-2 mt-1">
                           <Input
                             readOnly
                             type={
@@ -221,28 +288,28 @@ export const StreamPage = () => {
                             value={link.url}
                             className="flex-1"
                           />
-
-                          {link.hideCredentials && (
-                            <Button
-                              onClick={() =>
-                                setShowAdvancedPasswords((prev) => ({
-                                  ...prev,
-                                  [idx]: !prev[idx],
-                                }))
-                              }
-                            >
-                              {showAdvancedPasswords[idx] ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}{" "}
-                              {showAdvancedPasswords[idx] ? (
-                                <EyeClosed />
-                              ) : (
-                                <Eye />
-                              )}
+                          <div className="flex flex-row gap-2 mt-1">
+                            {link.hideCredentials && (
+                              <Button
+                                onClick={() =>
+                                  setShowAdvancedPasswords((prev) => ({
+                                    ...prev,
+                                    [idx]: !prev[idx],
+                                  }))
+                                }
+                              >
+                                {showAdvancedPasswords[idx] ? <TranslatedText id="common.hide"/> : <TranslatedText id="common.show"/>}{" "}
+                                {showAdvancedPasswords[idx] ? (
+                                  <EyeClosed />
+                                ) : (
+                                  <Eye />
+                                )}
+                              </Button>
+                            )}
+                            <Button onClick={() => copyToClipboard(link.url, t("common.copied"))}>
+                              <TranslatedText id="common.copy"/> <Copy />
                             </Button>
-                          )}
-
-                          <Button onClick={() => copyToClipboard(link.url, t("common.copied"))}>
-                            <TranslatedText id="common.copy"/> <Copy />
-                          </Button>
+                          </div>
                         </div>
                       </div>
                     ))}
