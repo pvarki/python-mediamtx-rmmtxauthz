9e057ad3e06136266123b426a71ba24daa1c8a35
TaikuriWeleho <228653559+TaikuriWeleho@users.noreply.github.com>
2026-01-09 08:20:11 +0200

    fix: correct docs link

--- src/rmmtxauthz/web/description.py
+++ src/rmmtxauthz/web/description.py
@@ -78,7 +78,7 @@ async def return_product_description_extended(language: str, request: Request) -
             icon="/ui/mtx/mtxlogo.svg",
             description="Videon suoratoistopalvelu",
             language=language,
-            docs="https://pvarki.github.io/Docusaurus-docs/docs/android/deployapp/home/",
+            docs="https://docs.pvarki.fi/",
             component=ProductComponent(type="component", ref=f"/ui/{shortname}/remoteEntry.js"),
         )
     if language == "sv":
@@ -88,7 +88,7 @@ async def return_product_description_extended(language: str, request: Request) -
             icon="/ui/mtx/mtxlogo.svg",
             description="Videoströmningstjänst",
             language=language,
-            docs="https://pvarki.github.io/Docusaurus-docs/docs/android/deployapp/home/",
+            docs="https://docs.pvarki.fi/",
             component=ProductComponent(type="component", ref=f"/ui/{shortname}/remoteEntry.js"),
         )
     return ProductDescriptionExtended(
@@ -97,6 +97,6 @@ async def return_product_description_extended(language: str, request: Request) -
         icon="/ui/mtx/mtxlogo.svg",
         description="Video streaming service",
         language=language,
-        docs="https://pvarki.github.io/Docusaurus-docs/docs/android/deployapp/home/",
+        docs="https://docs.pvarki.fi/",
         component=ProductComponent(type="component", ref=f"/ui/{shortname}/remoteEntry.js"),
     )
