a5a102a3a8ef0eaa4f6fdf4fdeca1891b33a9e7e
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-04-11 11:16:46 +0300

    feat: audit logging, redact password from logs

--- src/rmmtxauthz/web/mediamtx.py
+++ src/rmmtxauthz/web/mediamtx.py
@@ -1,6 +1,6 @@
 """MediaMTX auth routes"""
 
-from typing import Optional
+from typing import Optional, Any, Dict
 import logging
 
 from fastapi import APIRouter, HTTPException, Response
@@ -16,6 +16,23 @@ LOGGER = logging.getLogger(__name__)
 mtxrouter = APIRouter()
 
 
+def authreq_redact(authreq: MTXAuthReq) -> MTXAuthReq:
+    """Redact secrets if present"""
+    copy = authreq.model_copy()
+    if copy.password:
+        copy.password = "REDACTED"  # nosec # pragma: allowlist secret
+    return copy
+
+
+def make_log_extra(authreq: MTXAuthReq, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
+    """Make extra logging info"""
+    ret = {
+        "authreq": authreq_redact(authreq).model_dump(),
+    }
+    ret.update(kwargs)
+    return ret
+
+
 def check_apiuser(authreq: MTXAuthReq) -> Optional[Response]:
     """Check if user is this API"""
     if not authreq.user or not authreq.password:
@@ -25,7 +42,7 @@ def check_apiuser(authreq: MTXAuthReq) -> Optional[Response]:
     if authreq.user != conf.api_username:
         return None
     if conf.api_password == "CHANGEME":  # pragma: allowlist secret ; # nosec
-        LOGGER.error("api_password has not been changed from default, disallowing use")
+        LOGGER.audit("api_password has not been changed from default, disallowing use")  # type: ignore[attr-defined]  # pylint: disable=C0301
         raise HTTPException(status_code=401)
     if authreq.password != conf.api_password:
         raise HTTPException(status_code=401)
@@ -40,7 +57,7 @@ async def check_productuser(authreq: MTXAuthReq) -> Optional[Response]:
     try:
         dbproduct = await Product.by_cn(authreq.user)
         if authreq.password != dbproduct.mtxpassword:
-            LOGGER.error("Wrong password for {}".format(authreq.user))
+            LOGGER.audit("Wrong password for {}".format(authreq.user), extra=make_log_extra(authreq))  # type: ignore[attr-defined]  # pylint: disable=C0301
             raise HTTPException(status_code=403)
         return Response(status_code=204)
     except (NotFound, Deleted):
@@ -56,22 +73,27 @@ async def check_rmuser(authreq: MTXAuthReq) -> Optional[Response]:
     try:
         dbuser = await User.by_username(authreq.user)
         if authreq.password != dbuser.mtxpassword:
-            LOGGER.error("Wrong password for {}".format(authreq.user))
+            LOGGER.audit("Wrong password for {}".format(authreq.user), extra=make_log_extra(authreq))  # type: ignore[attr-defined]  # pylint: disable=C0301
             raise HTTPException(status_code=403)
+        if dbuser.is_rmadmin:
+            LOGGER.audit("{} is admin, allowing everything".format(authreq.user), extra=make_log_extra(authreq))  # type: ignore[attr-defined]  # pylint: disable=C0301
+            return Response(status_code=204)
         # Operations that require admin privileges
         if authreq.action in ("api", "metrics", "pprof") and not dbuser.is_rmadmin:
-            LOGGER.error("{} is not admin requesting {}".format(authreq.user, authreq.action))
+            LOGGER.audit(  # type: ignore[attr-defined]
+                "{} is not admin requesting {}".format(authreq.user, authreq.action), extra=make_log_extra(authreq)
+            )
             raise HTTPException(status_code=403)
         return Response(status_code=204)
     except (NotFound, Deleted) as exc:
-        LOGGER.error("Invalid user {}: {}".format(authreq.user, exc))
+        LOGGER.audit("Invalid user {}: {}".format(authreq.user, exc), extra=make_log_extra(authreq))  # type: ignore[attr-defined]  # pylint: disable=C0301
         raise HTTPException(status_code=403) from exc
 
 
 @mtxrouter.post("/auth")
 async def get_auth(authreq: MTXAuthReq) -> Response:
     """Check if username and password match and return empty ok if so"""
-    LOGGER.debug("Processing {}".format(authreq))
+    LOGGER.debug("Processing {}".format(authreq_redact(authreq)))
     if not authreq.user or not authreq.password:
         LOGGER.debug("No user/password, returning 401")
         raise HTTPException(status_code=401)
@@ -81,5 +103,5 @@ async def get_auth(authreq: MTXAuthReq) -> Response:
         return resp
     if resp := await check_rmuser(authreq):
         return resp
-    LOGGER.error("Fell through the checks, this should not happen")
+    LOGGER.audit("Fell through the checks, this should not happen", extra=make_log_extra(authreq))  # type: ignore[attr-defined]  # pylint: disable=C0301
     raise HTTPException(status_code=403)
