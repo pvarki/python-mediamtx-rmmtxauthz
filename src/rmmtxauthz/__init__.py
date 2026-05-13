6b55f72e9e47dcaad6cea3dc44677636f09adb30
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-05-13 12:24:36 +0300

    chore: bump version to 1.6.0+260513

--- src/rmmtxauthz/__init__.py
+++ src/rmmtxauthz/__init__.py
@@ -1,3 +1,3 @@
 """Do HTTP based authz for MediaMTX"""
 
-__version__ = "1.5.0+260427"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.6.0+260513"  # NOTE Use `bump-my-version` to bump versions correctly
