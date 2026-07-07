2d15900ab1ee1aa593226922605deccdaf4ba0be
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-07-07 10:55:02 +0300

    chore: bump version to 1.6.2+260707

--- tests/test_rmmtxauthz.py
+++ tests/test_rmmtxauthz.py
@@ -6,7 +6,7 @@ from rmmtxauthz.config import RMMTXSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.6.1+260612"
+    assert __version__ == "1.6.2+260707"
 
 
 def test_config() -> None:
